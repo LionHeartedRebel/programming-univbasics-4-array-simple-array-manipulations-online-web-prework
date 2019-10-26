30a72e7baea535c1de10dc4cb9f9f44d93129876
Andy choe <lionhearted.rebels@gmail.com>
2019-10-26 16:20:29 +0000

    Done.

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Introduction To Simple Array Manipulations
+.  oh # Introduction To Simple Array Manipulations
 
 ## Learning Goals
 
---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,21 @@
+def using_push (array, element)
+colors_in_the_rainbow = ["red", "orange", "yellow", "green", "blue", "indigo"]
+    next_color = "violet"
+    colors_in_the_rainbow.push(next_color)
+    end
+
+    
+def using_unshift (array, element)
+   bouroughs_in_nyc = ["Brooklyn", "Queens", "Manhattan", "Bronx"]
+   new_neighborhood = "Staten Island"
+   bouroughs_in_nyc.unshift(new_neighborhood)
+ end
+ 
+ def using_pop (array)
+    element = array.pop
+ end
+ 
+def pop_with_args (array)
+  element = array.pop(2)
+  end 
+  
