22d58eef75a670b15aa246893b189405c917278e
Andy choe <lionhearted.rebels@gmail.com>
2019-10-29 00:01:44 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -19,3 +19,20 @@ def pop_with_args (array)
   element = array.pop(2)
   end 
   
+def using_shift (array)
+  element = array.shift
+end
+
+def shift_with_args (array)
+  element = array.shift(2)
+end
+
+def using_concat (array, element)
+   array.concat(element)
+end
+
+def using_insert (array, element)
+ element = array.insert[4]
+end
+  
+  
